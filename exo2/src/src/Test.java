d5812fe8c2aa407d284cd5da4d55322c1a5aa503
Moren Yohan <yohan.moren@gmail.com>
2024-01-11 16:59:47 +0100

    Exo2 update

--- exo2/src/src/Test.java
+++ exo2/src/src/Test.java
@@ -1,5 +1,5 @@
 public class Test {
-    public void main(String[] arg) {
+    public static void main(String[] arg) { // Le test est déplacé dans un autre fichier pour être exécuté et il doit être static pour être exécuté
         Liste l1 = new Liste(5, new Liste(6, null));
         l1 = Liste.ajouter(18, l1); // Pour les lignes 70 - 71 et 73 il faut rajouter Liste a la place de l1 car il ne peut pas se redéclarer a chaque fois en s'utilisant soit même
         l1 = Liste.ajouter(25, l1);
